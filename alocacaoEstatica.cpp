5224c281cb6f04d2c64addce5e3b927b1315986e
Caio <caiovinius@hotmail.com>
2023-05-16 17:19:00 -0300

    feat: add RANDOM function to alocacaoEstatica

--- alocacaoEstatica.cpp
+++ alocacaoEstatica.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <random>
+#include <filesystem>
 
 using namespace std;
 
-const int MAX_SIZE = 100;
+namespace fs = std::filesystem;
+
+const int MAX_SIZE = 1000;
 
 struct Node {
     int data;
@@ -39,17 +45,55 @@ void printTable(Node** table, int size) {
     }
 }
 
+std::string getRandomFile(const std::string& directory) {
+    std::vector<std::string> fileNames;
+    
+    // Percorre todos os arquivos do diretório
+    for (const auto& entry : fs::directory_iterator(directory)) {
+        if (entry.is_regular_file()) {
+            std::string fileName = entry.path().filename().string();
+            
+            // Verifica se o formato do nome do arquivo está correto
+            if (fileName.length() >= 13 && fileName.substr(fileName.length() - 10) == "-input.txt") {
+                fileNames.push_back(fileName);
+            }
+        }
+    }
+    
+    // Verifica se existem arquivos que seguem o formato especificado
+    if (fileNames.empty()) {
+        std::cout << "Nenhum arquivo válido encontrado no diretório." << std::endl;
+        return "";
+    }
+    
+    // Gera um índice aleatório para selecionar um arquivo
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> uni(0, fileNames.size() - 1);
+    int randomIndex = uni(rng);
+    
+    // Retorna o nome do arquivo selecionado
+    return fileNames[randomIndex];
+}
+
 int main() {
-    ifstream inputFile("entrada.txt");
-    if (!inputFile) {
-        cout << "Erro ao abrir o arquivo de entrada." << endl;
+    std::string directory = "entradas";
+    std::string inputFile = directory + "/" + getRandomFile(directory);
+
+    ifstream inFile(inputFile);
+    if (!inFile.is_open()) {
+        cout << "Erro ao abrir o arquivo de entrada: " << inputFile << endl;
         return 1;
     }
 
+    std::cout << "Arquivo selecionado: " << inputFile << std::endl;
+
     int size;
-    inputFile >> size;
+    inFile >> size;
+    cout << "Tamanho da tabela: " << size << endl;
     if (size <= 0 || size > MAX_SIZE) {
         cout << "Tamanho inválido para a tabela." << endl;
+        inFile.close();
         return 1;
     }
 
@@ -59,13 +103,13 @@ int main() {
     }
 
     int value;
-    while (inputFile >> value) {
+    while (inFile >> value) {
         insert(table, size, value);
     }
 
     printTable(table, size);
 
-    inputFile.close();
+    inFile.close();
 
     return 0;
 }
